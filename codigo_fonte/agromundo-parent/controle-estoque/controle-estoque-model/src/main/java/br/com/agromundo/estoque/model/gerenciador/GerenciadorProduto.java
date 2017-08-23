931ceb43f683cf506417f0ff6773dcf464065116
Leonardo Borges <odranoel.df@gmail.com>
2017-08-23 12:48:25 -0300

    add gerenciador

---
+++ codigo_fonte/agromundo-parent/controle-estoque/controle-estoque-model/src/main/java/br/com/agromundo/estoque/model/gerenciador/GerenciadorProduto.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package br.com.agromundo.estoque.model.gerenciador;
+
+import br.com.agromundo.estoque.model.dominio.Produto;
+
+/**
+ * @author Leonardo Borges
+ *
+ */
+public class GerenciadorProduto {
+
+  /**
+   * @param entidade
+   */
+  public void cadastrar(Produto entidade) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /**
+   * @param token
+   * @param idTipoToken
+   */
+  public void excluir(String token, int idTipoToken) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
